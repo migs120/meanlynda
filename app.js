850c80619d0097c5156c9b37adda9bd0493f655d
Your Name <you@example.com>
2015-07-01 13:48:43 +0000

    doneSec2vid3-erONlocalhost

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+
+var config = require('./config');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var orders = require('./routes/orders');
+
+mongoose.connect(config.mongoUri);
 
 var app = express();
 
