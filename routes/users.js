00a7bae076b18e29300549e33e99f0b632f3dd32
Your Name <you@example.com>
2015-07-01 11:55:30 +0000

    sec2vid1min5

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+/* GET users/views */
+router.get('/create', function(req, res, next) {
+  
+  var vm =  {
+            title:'create an account'
+            };
+  res.render('users/create', vm);
+});
+
+
 module.exports = router;
